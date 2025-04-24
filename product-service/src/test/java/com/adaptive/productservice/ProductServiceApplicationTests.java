f4d5c1c92316fed94561704b8cd8f7899248040c
z.khaddan <zakaria.khaddan@it-adaptive.com>
2025-04-24 14:42:47 +0100

    first commit

---
+++ product-service/src/test/java/com/adaptive/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaptive.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
